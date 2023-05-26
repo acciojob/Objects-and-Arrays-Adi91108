6fe6007fecaff324db28982d27ce3358de89363f
Aditya sen <130759840+Adi91108@users.noreply.github.com>
2023-05-26 20:21:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,6 +7,16 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
+const team = players;
+const team1=[...players];
+// Copying the object
+const cap1 = person;
+
+// Logging the results
+
+// console.log(team1);
+// console.log(cap1);
+
 window.players = players;
 window.person = person;
 window.team = team;
